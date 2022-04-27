1f2f1fb41f168d50586d3e07a9da8ad334952cda
Daria Tuzhilina <dtuzhilina@fors.ru>
2022-04-28 01:01:12 +0300

    feat: decided hanoi-tower, simple-chain, transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create transformed array based on the control sequences that original
  * array contains
@@ -13,9 +11,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let res = [];
+
+  if(!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!')
+  }
+
+  for(let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        if(res.length > 0 && arr[i - 2] !== '--discard-next') {
+          res.pop();
+        }
+        break;
+      case '--double-next':
+        if(arr[i + 1]) {
+          res.push(arr[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        if(res.length > 0 && arr[i - 2] !== '--discard-next') {
+          res.push(arr[i - 1]);
+        }
+        break;
+      default:
+        res.push(arr[i]);
+    }
+  }
+
+  return res;
 }
 
 module.exports = {
