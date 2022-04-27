b12e3dc14c25f952cefb9c2c095eae431459166c
Daria Tuzhilina <dtuzhilina@fors.ru>
2022-04-27 11:02:38 +0300

    feat: decided what-season

--- src/what-season.js
+++ src/what-season.js
@@ -12,36 +12,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  if (date instanceof Date) {
 
-    // tricky moment
-    if(!date.getTime) {
-      return 'Invalid date!';
-    }
-
-    const month = date.getMonth();
-
-    switch (month) {
-      case 11:
-      case 0:
-      case 1:
-        return 'winter';
-      case 2:
-      case 3:
-      case 4:
-        return 'spring';
-      case 5:
-      case 6:
-      case 7:
-        return 'summer';
-      case 8:
-      case 9:
-      case 10:
-        return 'autumn';
-    }
+  if (!date) {
+    return 'Unable to determine the time of year!';
   }
 
-  return date ? 'Invalid date!' : 'Unable to determine the time of year!';
+  if(!(date instanceof Date)) {
+    throw new Error('Invalid date!');
+  }
+
+  // tricky moment
+  try {
+    date.getTime();
+  } catch (e) {
+    throw new Error('Invalid date!');
+  }
+
+  switch (date.getMonth()) {
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
+  }
 }
 
 module.exports = {
