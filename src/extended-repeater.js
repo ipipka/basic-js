73185aab23eb41553f1e577ad8cc029c44f68d8a
Daria Tuzhilina <dtuzhilina@fors.ru>
2022-04-28 03:26:05 +0300

    feat: decided extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create a repeating string based on the given parameters
  *  
@@ -15,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const repeatTimes = options?.repeatTimes || 1;
+  const separator = options?.separator || '+';
+  const addition = options.hasOwnProperty('addition') ? String(options.addition) : '';
+  const additionRepeatTimes = options?.additionRepeatTimes || 1;
+  const additionSeparator = options.hasOwnProperty('additionSeparator') ? String(options.additionSeparator) : '|';
+
+  let resAddition = '';
+  if(addition) {
+    resAddition = new Array(additionRepeatTimes).fill(addition);
+    resAddition = resAddition.join(additionSeparator);
+  }
+
+  let res = new Array(repeatTimes).fill(`${str}${resAddition}`)
+  return res.join(separator);
 }
 
 module.exports = {
